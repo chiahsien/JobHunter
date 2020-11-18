1827f481cecc6f364becf95753db688a9f25147a
Nelson Tai <chiahsien@gmail.com>
2020-11-18 23:23:18 +0800

    Initial commit

---
+++ JobHunter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobHunter
+//
+//  Created by Nelson on 2020/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, "14.0"
+inhibit_all_warnings!
+use_frameworks!
+
+target 'JobHunter' do
+  pod 'SwiftSoup'
+end
